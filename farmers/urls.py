bcb2afd98d5c6e94d7ad7e788f90d898afeb4cde
Olti-B <oltidev.18@gmail.com>
2018-09-05 09:56:53 +0200

    formating html and adding ninja

--- farmers/urls.py
+++ farmers/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/register', views.index, name='index'),
 
 ]
--- incubator/urls.py
+++ incubator/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('farmers/', include('farmers.urls')),
+    path('', include('farmers.urls')),
 ]
