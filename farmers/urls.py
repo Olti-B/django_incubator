6461225e11d7e85cc4cf996ac49ab477c88e7384
Olti-B <oltidev.18@gmail.com>
2018-09-07 10:13:32 +0200

    creating the main route

--- farmers/urls.py
+++ farmers/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/register', views.index, name='index'),
+    path('register', views.register, name='register'),
+    path('products', views.products, name='products'),
+    path('land', views.land, name='land'),
+    path('invest', views.invest, name='invest'),
+    path('plant_register', views.plant_register, name='plant_register'),
+    path('profile', views.profile, name='profile'),
 
 ]
--- farmers/views.py
+++ farmers/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(req):
     return render(req, 'farmers/home.html')
+
+
+def register(req):
+    return render(req, 'farmers/register.html')
+
+
+def products(req):
+    return render(req, 'farmers/products.html')
+
+
+def land(req):
+    return render(req, 'farmers/land.html')
+
+
+def invest(req):
+    return render(req, 'farmers/invest.html')
+
+
+def plant_register(req):
+    return render(req, 'farmers/plant_register.html')
+
+
+def profile(req):
+    return render(req, 'farmers/profile.html')
